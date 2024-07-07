2030ce485c83cbf6d190e3f627fdf7d72dca22de
Gheffari_Youcef <youcefgheffari3@gmail.com>
2024-07-07 01:50:59 +0100

    update_2

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,89 +1,53 @@
 import pandas as pd
 from glob import glob
 
-# --------------------------------------------------------------
-# Read single CSV file
-# --------------------------------------------------------------
-single_file_acc = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv')
-
-single_file_gyr = pd.read_csv('../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv')
-# --------------------------------------------------------------
-# List all data in data/raw/MetaMotion
-# --------------------------------------------------------------
-
-files = glob('../../data/raw/MetaMotion/*.csv')
-
-len(files)
-# --------------------------------------------------------------
-# Extract features from filename
-# --------------------------------------------------------------
 data_path = '../../data/raw/MetaMotion/'
-f = files[0]
-participant = f.split('-')[0].replace(data_path, '')
-label = f.split('-')[1]
-category = f.split('-')[2].rstrip('_MetaWear_2019').rstrip('123')
-
-df = pd.read_csv(f)
-df['participant'] = participant
-df['label'] = label
-df['category'] = category
-
-
-
-
-# --------------------------------------------------------------
-# Read all files
-# --------------------------------------------------------------
-
-acc_df = pd.DataFrame()
-gyr_df = pd.DataFrame()
-
-acc_set = 1
-gyr_set = 1
-
-for f in files :
-    participant = f.split('-')[0].replace(data_path, '')
-    label = f.split('-')[1]
-    category = f.split('-')[2].rstrip('_MetaWear_2019').rstrip('123')
-
-    df = pd.read_csv(f)
-    df['participant'] = participant
-    df['label'] = label
-    df['category'] = category
-
-    if 'Gyroscope' in f :
-        df['set'] = gyr_set
-        gyr_set += 1
-        gyr_df = pd.concat([gyr_df, df])
-    
-    if 'Accelerometer' in f :
-        df['set'] = acc_set
-        acc_set += 1
-        acc_df = pd.concat([acc_df, df])
-
-# --------------------------------------------------------------
-# Working with datetimes
-# --------------------------------------------------------------
-acc_df.info()
-
-pd.to_datetime(acc_df['epoch (ms)'], unit='ms')
-
-acc_df.index = pd.to_datetime(acc_df['epoch (ms)'], unit='ms')
-gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'], unit='ms')
-
-del acc_df['epoch (ms)']
-del acc_df['time (01:00)']
-del acc_df['elapsed (s)']
-
-del gyr_df['epoch (ms)']
-del gyr_df['time (01:00)']
-del gyr_df['elapsed (s)']
-
-
 # --------------------------------------------------------------
 # Turn into function
 # --------------------------------------------------------------
+files = glob('../../data/raw/MetaMotion/*.csv')
 
+def read_data_from_files(files) :
+    acc_df = pd.DataFrame()
+    gyr_df = pd.DataFrame()
+
+    acc_set = 1
+    gyr_set = 1
+
+    for f in files :
+        participant = f.split('-')[0].replace(data_path, '')
+        label = f.split('-')[1]
+        category = f.split('-')[2].rstrip('_MetaWear_2019').rstrip('123')
+
+        df = pd.read_csv(f)
+        df['participant'] = participant
+        df['label'] = label
+        df['category'] = category
+
+        if 'Gyroscope' in f :
+            df['set'] = gyr_set
+            gyr_set += 1
+            gyr_df = pd.concat([gyr_df, df])
+        
+        if 'Accelerometer' in f :
+            df['set'] = acc_set
+            acc_set += 1
+            acc_df = pd.concat([acc_df, df])
+    acc_df.index = pd.to_datetime(acc_df['epoch (ms)'], unit='ms')
+    gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'], unit='ms')
+
+    del acc_df['epoch (ms)']
+    del acc_df['time (01:00)']
+    del acc_df['elapsed (s)']
+
+    del gyr_df['epoch (ms)']
+    del gyr_df['time (01:00)']
+    del gyr_df['elapsed (s)']
+    
+    return acc_df, gyr_df
+    
+acc_df, gyr_df = read_data_from_files(files)   
+    
 
 # --------------------------------------------------------------
 # Merging datasets
