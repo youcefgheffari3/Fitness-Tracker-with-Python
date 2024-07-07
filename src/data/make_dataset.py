09846241e1506dd586c6d5bb7ec609e4b469c852
Gheffari_Youcef <youcefgheffari3@gmail.com>
2024-07-07 01:42:52 +0100

    update_1

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -38,12 +38,46 @@ df['category'] = category
 acc_df = pd.DataFrame()
 gyr_df = pd.DataFrame()
 
+acc_set = 1
+gyr_set = 1
 
+for f in files :
+    participant = f.split('-')[0].replace(data_path, '')
+    label = f.split('-')[1]
+    category = f.split('-')[2].rstrip('_MetaWear_2019').rstrip('123')
 
+    df = pd.read_csv(f)
+    df['participant'] = participant
+    df['label'] = label
+    df['category'] = category
+
+    if 'Gyroscope' in f :
+        df['set'] = gyr_set
+        gyr_set += 1
+        gyr_df = pd.concat([gyr_df, df])
+    
+    if 'Accelerometer' in f :
+        df['set'] = acc_set
+        acc_set += 1
+        acc_df = pd.concat([acc_df, df])
 
 # --------------------------------------------------------------
 # Working with datetimes
 # --------------------------------------------------------------
+acc_df.info()
+
+pd.to_datetime(acc_df['epoch (ms)'], unit='ms')
+
+acc_df.index = pd.to_datetime(acc_df['epoch (ms)'], unit='ms')
+gyr_df.index = pd.to_datetime(gyr_df['epoch (ms)'], unit='ms')
+
+del acc_df['epoch (ms)']
+del acc_df['time (01:00)']
+del acc_df['elapsed (s)']
+
+del gyr_df['epoch (ms)']
+del gyr_df['time (01:00)']
+del gyr_df['elapsed (s)']
 
 
 # --------------------------------------------------------------
